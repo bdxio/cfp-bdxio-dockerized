55149d1ca6e5940cce94b1be9687dc56e6b13222
Frederic Camblor <fcamblor@gmail.com>
2014-05-16 12:59:12 +0000

    Moved docker build after potential docker stop in order to avoid chicken & egg problem

--- runme.sh
+++ runme.sh
@@ -9,12 +9,12 @@ function dockerBuildAndPrepareRun() {
   img_name=$1
   img_alias=$2
   img_dir=$3
-  docker build -t $USERNAME/$img_name $img_dir
   if [ $(docker ps | grep $USERNAME/$img_name | wc -l) -ne 0 ]; then
     img_uuid=$(docker ps | grep $USERNAME/$img_name | cut -d" " -f1)
     docker stop $img_uuid
   fi
   docker rm $img_alias
+  docker build -t $USERNAME/$img_name $img_dir
 }
 
 # Building then running elastic search container
