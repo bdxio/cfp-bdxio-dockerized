3bdb39c3fb0019a749bef97926faf7d56b8ad506
Frederic Camblor <fred@bdx.io>
2015-05-01 23:35:00 +0000

    Retrieving es & redis docker instance name after run, allowing to be more reliable than a docker ps + grep

--- run-containers.sh
+++ run-containers.sh
@@ -19,13 +19,11 @@ function extractApplicationConfig(){
 }
 
 docker run -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock jwilder/nginx-proxy
-docker --debug run -d -p 49200:9200 -p 49300:9300 $USERNAME/cfp-elasticsearch
-docker --debug run -d -p 6363:6379 -v $CURRENT_DIR/backups/prod:/data -v $CURRENT_DIR/logs:/var/log/redis -v $CURRENT_DIR/redis/redis-config-prod.conf:/etc/redis.conf redis:2.8 redis-server /etc/redis.conf
 
 # Note using the -name option on previous docker commands because a "named" container is harder to remove
 # than a randomly-named container (and we cannot reuse an already-used name)
-redisContainerName=$(docker ps | grep 'cfp-redis' | sed -e 's/.*[ ]\([^ ]\{1,\}\)[ ]*$/\1/g' | cut -d "," -f1)
-esContainerName=$(docker ps | grep 'cfp-elasticsearch' | sed -e 's/.*[ ]\([^ ]\{1,\}\)[ ]*$/\1/g' | cut -d "," -f1)
+esContainerName=$(docker --debug run -d -p 49200:9200 -p 49300:9300 $USERNAME/cfp-elasticsearch)
+redisProdContainerName=$(docker --debug run -d -p 6363:6379 -v $CURRENT_DIR/backups/prod:/data -v $CURRENT_DIR/logs:/var/log/redis -v $CURRENT_DIR/redis/redis-config-prod.conf:/etc/redis.conf redis:2.8 redis-server /etc/redis.conf)
 
 git submodule update
 extractApplicationConfig $APP_CONFIG_ENV
