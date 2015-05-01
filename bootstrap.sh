f55142966ba641d9edcb87b9410a61d85613be43
Frederic Camblor <fred@bdx.io>
2015-05-01 23:09:13 +0000

    Creating backups sub directories during bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,13 @@ git submodule update
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+mkdir --parent $CURRENT_DIR/backups/prod/
+mkdir --parent $CURRENT_DIR/backups/testing/
+mkdir --parent $CURRENT_DIR/logs/prod/
+mkdir --parent $CURRENT_DIR/logs/testing/
+
+chmod 777 $CURRENT_DIR/logs/prod/ $CURRENT_DIR/logs/testing/
+
 REDIS_CONFIG_REPO=git@gitlab.com:bdxio/cfp-bdx-io.git
 REDIS_CONFIG_REPO_BRANCH=configurations
 
