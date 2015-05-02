085f3e6f5b1cdfdc93186dfcc5ddc02b68b643ec
Frederic Camblor <fred@bdx.io>
2015-05-02 00:03:26 +0000

    Fix docker webapp name during run

--- run-containers.sh
+++ run-containers.sh
@@ -15,5 +15,5 @@ esContainerName=$(docker --debug run -d -p 49200:9200 -p 49300:9300 $USERNAME/cf
 redisProdContainerName=$(docker --debug run -d -p 6363:6379 -v $CURRENT_DIR/backups/prod:/data -v $CURRENT_DIR/logs/prod:/var/log/redis -v $CURRENT_DIR/redis/redis-config-prod.conf:/etc/redis.conf redis:2.8 redis-server /etc/redis.conf)
 redisTestingContainerName=$(docker --debug run -d -p 6364:6379 -v $CURRENT_DIR/backups/testing:/data -v $CURRENT_DIR/logs/testing:/var/log/redis -v $CURRENT_DIR/redis/redis-config-testing.conf:/etc/redis.conf redis:2.8 redis-server /etc/redis.conf)
 
-docker run -d --link $esContainerName:es --link $redisProdContainerName:redis -e "VIRTUAL_HOST=cfp.bdx.io" -v $CURRENT_DIR/cfp-src-prod:/cfp-src/ $USERNAME/cfp-webapp-prod
-docker run -d --link $esContainerName:es --link $redisTestingContainerName:redis -e "VIRTUAL_HOST=cfp-testing.bdx.io" -v $CURRENT_DIR/cfp-src-testing:/cfp-src/ $USERNAME/cfp-webapp-testing
+docker run -d --link $esContainerName:es --link $redisProdContainerName:redis -e "VIRTUAL_HOST=cfp.bdx.io" -v $CURRENT_DIR/cfp-src-prod:/cfp-src/ $USERNAME/cfp-webapp
+docker run -d --link $esContainerName:es --link $redisTestingContainerName:redis -e "VIRTUAL_HOST=cfp-testing.bdx.io" -v $CURRENT_DIR/cfp-src-testing:/cfp-src/ $USERNAME/cfp-webapp
