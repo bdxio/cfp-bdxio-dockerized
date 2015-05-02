39972e7ca3bb8b6e35e0ddab0e12ae14f9ac74cb
Frederic Camblor <fred@bdx.io>
2015-05-02 22:21:48 +0000

    Revert "Added --rm flag on docker runs" since it's not compatible with -d
    This reverts commit ce46b4529379a6ec657032f4dcf842b01683f7ad.

--- run-containers.sh
+++ run-containers.sh
@@ -7,15 +7,15 @@ fi;
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker run --rm -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock jwilder/nginx-proxy
+docker run -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock jwilder/nginx-proxy
 
 # Note using the -name option on previous docker commands because a "named" container is harder to remove
 # than a randomly-named container (and we cannot reuse an already-used name)
-esContainerName=$(docker --debug run --rm -d -p 49200:9200 -p 49300:9300 $USERNAME/cfp-elasticsearch)
+esContainerName=$(docker --debug run -d -p 49200:9200 -p 49300:9300 $USERNAME/cfp-elasticsearch)
 redisProdContainerName=$(docker --debug run -d -p 6363:6379 -v $CURRENT_DIR/dropbox/cfp-backups/prod:/data -v $CURRENT_DIR/logs/prod:/var/log/redis -v $CURRENT_DIR/redis/redis-config-prod.conf:/etc/redis.conf redis:2.8 redis-server /etc/redis.conf)
 redisTestingContainerName=$(docker --debug run -d -p 6364:6379 -v $CURRENT_DIR/dropbox/cfp-backups/testing:/data -v $CURRENT_DIR/logs/testing:/var/log/redis -v $CURRENT_DIR/redis/redis-config-testing.conf:/etc/redis.conf redis:2.8 redis-server /etc/redis.conf)
 
-docker run --rm -d --link $esContainerName:es --link $redisProdContainerName:redis -e "VIRTUAL_HOST=cfp.bdx.io" -v $CURRENT_DIR/cfp-src-prod:/cfp-src/ $USERNAME/cfp-webapp
-docker run --rm -d --link $esContainerName:es --link $redisTestingContainerName:redis -e "VIRTUAL_HOST=cfp-testing.bdx.io" -v $CURRENT_DIR/cfp-src-testing:/cfp-src/ $USERNAME/cfp-webapp
+docker run -d --link $esContainerName:es --link $redisProdContainerName:redis -e "VIRTUAL_HOST=cfp.bdx.io" -v $CURRENT_DIR/cfp-src-prod:/cfp-src/ $USERNAME/cfp-webapp
+docker run -d --link $esContainerName:es --link $redisTestingContainerName:redis -e "VIRTUAL_HOST=cfp-testing.bdx.io" -v $CURRENT_DIR/cfp-src-testing:/cfp-src/ $USERNAME/cfp-webapp
 
-docker run --rm -dv $CURRENT_DIR/dropbox:/Dropbox my-dropbox /.dropbox-dist/dropboxd
+docker run -dv $CURRENT_DIR/dropbox:/Dropbox my-dropbox /.dropbox-dist/dropboxd
