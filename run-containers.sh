ff34d835a9c31b25ae69aaaa1326095423edb631
Frederic Camblor <fred@bdx.io>
2015-06-04 22:43:12 +0000

    Fixed docker startups for redis and es

--- run-containers.sh
+++ run-containers.sh
@@ -13,10 +13,13 @@ docker run -d --privileged=true -v $CURRENT_DIR/dropbox:/home/Dropbox -v /etc/lo
 
 # Note using the -name option on previous docker commands because a "named" container is harder to remove
 # than a randomly-named container (and we cannot reuse an already-used name)
-esProdContainerName=$(docker run -d -p 49200:9200 -p 49300:9300 -v $CURRENT_DIR/dropbox/cfp-backups/prod/es:/usr/share/elasticsearch/data elasticsearch)
-esTestingContainerName=$(docker run -d -p 59200:9200 -p 59300:9300 -v $CURRENT_DIR/dropbox/cfp-backups/testing/es:/usr/share/elasticsearch/data elasticsearch)
-redisProdContainerName=$(docker --debug run -d -p 6363:6379 -v $CURRENT_DIR/dropbox/cfp-backups/prod/redis:/data -v $CURRENT_DIR/logs/prod:/var/log/redis -v $CURRENT_DIR/redis/redis-config-prod.conf:/etc/redis.conf redis:2.8 redis-server /etc/redis.conf)
-redisTestingContainerName=$(docker --debug run -d -p 6364:6379 -v $CURRENT_DIR/dropbox/cfp-backups/testing/redis:/data -v $CURRENT_DIR/logs/testing:/var/log/redis -v $CURRENT_DIR/redis/redis-config-testing.conf:/etc/redis.conf redis:2.8 redis-server /etc/redis.conf)
+sleep 2 # Dunno why but without waiting around elasticsearch docker images, es will immediately stop
+esProdContainerName=$(docker run -d -p 49200:9200 -p 49300:9300 -v $CURRENT_DIR/dropbox/cfp-backups/prod/es:/usr/share/elasticsearch/data/ elasticsearch)
+sleep 2 # Dunno why but without waiting around elasticsearch docker images, es will immediately stop
+esTestingContainerName=$(docker run -d -p 59200:9200 -p 59300:9300 -v $CURRENT_DIR/dropbox/cfp-backups/testing/es:/usr/share/elasticsearch/data/ elasticsearch)
+sleep 2 # Dunno why but without waiting around elasticsearch docker images, es will immediately stop
+redisProdContainerName=$(docker --debug run -d -p 6363:6379 -v $CURRENT_DIR/dropbox/cfp-backups/prod/redis:/data/ -v $CURRENT_DIR/logs/prod:/var/log/redis/ -v $CURRENT_DIR/redis/redis-config-prod.conf:/etc/redis.conf redis:2.8 redis-server /etc/redis.conf)
+redisTestingContainerName=$(docker --debug run -d -p 6364:6379 -v $CURRENT_DIR/dropbox/cfp-backups/testing/redis:/data/ -v $CURRENT_DIR/logs/testing:/var/log/redis/ -v $CURRENT_DIR/redis/redis-config-testing.conf:/etc/redis.conf redis:2.8 redis-server /etc/redis.conf)
 
 docker run -d --link $esProdContainerName:es --link $redisProdContainerName:redis -e "VIRTUAL_HOST=cfp.bdx.io" -v $CURRENT_DIR/cfp-src-prod:/cfp-src/ --name cfp-webapp-prod $USERNAME/cfp-webapp
 docker run -d --link $esTestingContainerName:es --link $redisTestingContainerName:redis -e "VIRTUAL_HOST=cfp-testing.bdx.io" -v $CURRENT_DIR/cfp-src-testing:/cfp-src/ --name cfp-webapp-testing $USERNAME/cfp-webapp
