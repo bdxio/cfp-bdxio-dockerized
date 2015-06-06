665618fbb77a951b38acb0f001acc7d10ba2928c
Frederic Camblor <fred@bdx.io>
2015-06-06 14:36:40 +0000

    Not sharing elasticsearch data dirs because we have permission write issues when doing so

--- run-containers.sh
+++ run-containers.sh
@@ -14,9 +14,9 @@ docker run -d --privileged=true -v $CURRENT_DIR/dropbox:/home/Dropbox -v /etc/lo
 # Note using the -name option on previous docker commands because a "named" container is harder to remove
 # than a randomly-named container (and we cannot reuse an already-used name)
 sleep 2 # Dunno why but without waiting around elasticsearch docker images, es will immediately stop
-esProdContainerName=$(docker run -d -p 49200:9200 -p 49300:9300 -v $CURRENT_DIR/dropbox/cfp-backups/prod/es:/usr/share/elasticsearch/data/ elasticsearch)
+esProdContainerName=$(docker run -d -p 49200:9200 -p 49300:9300 elasticsearch)
 sleep 2 # Dunno why but without waiting around elasticsearch docker images, es will immediately stop
-esTestingContainerName=$(docker run -d -p 59200:9200 -p 59300:9300 -v $CURRENT_DIR/dropbox/cfp-backups/testing/es:/usr/share/elasticsearch/data/ elasticsearch)
+esTestingContainerName=$(docker run -d -p 59200:9200 -p 59300:9300 elasticsearch)
 sleep 2 # Dunno why but without waiting around elasticsearch docker images, es will immediately stop
 redisProdContainerName=$(docker --debug run -d -p 6363:6379 -v $CURRENT_DIR/dropbox/cfp-backups/prod/redis:/data/ -v $CURRENT_DIR/logs/prod:/var/log/redis/ -v $CURRENT_DIR/redis/redis-config-prod.conf:/etc/redis.conf redis:2.8 redis-server /etc/redis.conf)
 redisTestingContainerName=$(docker --debug run -d -p 6364:6379 -v $CURRENT_DIR/dropbox/cfp-backups/testing/redis:/data/ -v $CURRENT_DIR/logs/testing:/var/log/redis/ -v $CURRENT_DIR/redis/redis-config-testing.conf:/etc/redis.conf redis:2.8 redis-server /etc/redis.conf)
