14d8fdd8770268f83cc59076aac3428f65d65dbe
Frederic Camblor <fred@bdx.io>
2016-06-03 10:03:17 +0000

    Fix start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@ docker-compose -f docker-compose-dropbox.yml up -d
 
 sleep 10
 
+docker logs $(docker ps -f name=dropbox | tail -n 1 | cut -d" " -f1)
+
 echo ""
 echo ""
 echo ""
 read -p "  Copy paste the dropbox url in your browser then hit ENTER to continue..."
-
-docker logs $(docker ps -f name=dropbox | tail -n 1 | cut -d" " -f1)
 
 echo "Starting CFP docker instances..."
 docker-compose up -d
