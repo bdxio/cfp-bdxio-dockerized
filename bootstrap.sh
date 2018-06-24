28b334e1c5e432e7dd6aec4e7ed49a593eec64de
Benoît Giraudou <bgiraudou@gmail.com>
2018-06-24 16:32:25 +0200

    🐳 Use Traefik as reverse proxy

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 git submodule init
 git submodule update
@@ -9,21 +9,22 @@ APP_CONFIG_REPO_BRANCH=configurations
 REDIS_CONFIG_REPO=git@gitlab.com:bdxio/cfp-bdx-io.git
 REDIS_CONFIG_REPO_BRANCH=configurations
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 mkdir $CURRENT_DIR/dropbox/
-mkdir $CURRENT_DIR/certs/
-mkdir $CURRENT_DIR/http-le-challenge/
-mkdir --parent $CURRENT_DIR/logs/prod/
-mkdir --parent $CURRENT_DIR/logs/testing/
-mkdir --parent $CURRENT_DIR/dropbox/cfp-backups/prod/redis/
-mkdir --parent $CURRENT_DIR/dropbox/cfp-backups/prod/es/
-mkdir --parent $CURRENT_DIR/dropbox/cfp-backups/testing/redis/
-mkdir --parent $CURRENT_DIR/dropbox/cfp-backups/testing/es/
+mkdir -p $CURRENT_DIR/logs/prod/
+mkdir -p $CURRENT_DIR/logs/testing/
+mkdir -p $CURRENT_DIR/dropbox/cfp-backups/prod/redis/
+mkdir -p $CURRENT_DIR/dropbox/cfp-backups/prod/es/
+mkdir -p $CURRENT_DIR/dropbox/cfp-backups/testing/redis/
+mkdir -p $CURRENT_DIR/dropbox/cfp-backups/testing/es/
 mkdir redis
 
 # This stuff is VERY important otherwise REDIS won't start at all
 chmod -R 777 $CURRENT_DIR/logs/prod/ $CURRENT_DIR/logs/testing/ $CURRENT_DIR/dropbox/
+
+touch acme.json
+chmod 0600 acme.json
 
 git archive --remote=$REDIS_CONFIG_REPO $REDIS_CONFIG_REPO_BRANCH redis-config-*.conf | tar -x; mv redis-config-*.conf $CURRENT_DIR/redis/
 
@@ -33,4 +34,3 @@ function extractApplicationConfig(){
 
 extractApplicationConfig prod
 extractApplicationConfig testing
-
